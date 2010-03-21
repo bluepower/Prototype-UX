024a47bcd205b6a60fdd827fa86aebf617f1b557
Niko Ni <bluepspower@163.com>
2010-03-22 01:20:49 +0800

    minor update

---
+++ sandbox/gcc-showcase/gcc-showcase.js
@@ -0,0 +1,105 @@
+/**
+ * @class Gcc.Showcase
+ * @author Niko Ni
+ * @create 2010-03-21
+ * @update 2010-03-22
+ */
+Gcc.Showcase = Class.create({
+	/**
+	 * @constructor
+	 */
+	initialize : function(config) {
+		this.opt = config || {};
+		this.items = this.opt['items'] || [];
+		this.bound = false;
+
+        // init markup
+        this.initMarkup();
+        // init events
+        this.initEvents();
+	},
+
+	initMarkup : function() {
+		this.items.each(function(item, index) {
+			item.id = 'sample-' + index;
+		});
+
+		this.menu = $(this.opt['menuEl']) || $('sample-menu-inner');
+		this.ct = $(this.opt['contentEl']) || $('sample-box-inner');
+		this.cb = $(this.opt['controlBarEl']) || $('samples-cb');
+
+		this.initTemplates();
+	},
+
+	initEvents : function() {
+		this.ct.observe('mouseover', function() {
+			//@TODO
+		});
+		this.ct.observe('mouseout', function() {
+			//@TODO
+		});
+		this.ct.observe('click', function() {
+			//@TODO
+		});
+
+		this.menu.observe('click', function(ev) {
+			ev.stop();
+			//@TODO
+		});
+
+		this.cb.observe('click', function() {
+			//@TODO
+		});
+
+		this.bindScroll(true);
+
+		this.activate(this.items[0].id);
+	},
+
+	initTemplates : function() {
+		//@TODO
+	},
+
+	calcScrollPosition : function() {
+		var last, found = false;
+		this.ct.select('a[name]').each(function(item) {
+			last = item;
+
+			if(item.getOffsetsTo(this.ct)[1] >= -10) {
+				this.activate(item.id);
+				found = true;
+				return false;
+			}
+		}.bind(this));
+
+		if(!found) {
+			this.activate(last.id);
+		}
+	},
+
+	bindScroll : function(flag) {
+		var fn = this.calcScrollPosition;
+		if(flag) {
+			this.ct.observe('scroll', fn);
+			this.bound = true;
+		} else {
+			this.ct.stopObserving('scroll', fn);
+			this.bound = false;
+		}
+	},
+
+	activate : function(id) {
+		var el = $('a4' + id);
+		this.radioClass(el, 'active');
+	},
+
+    radioClass : function(el, cls) {
+        if(el) {
+            var arr = el.up().childElements();
+            arr.each(function(item) {
+                item.removeClassName(cls);
+            });
+            el.addClassName(cls);
+        }
+    }
+});
